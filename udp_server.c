d0e94a7cc40892db845b4e0cd91a7284e7b7d442
sbhakim <sbhakim@kth.se>
2017-10-07 20:35:59 +0300

    modified the whole file

--- udp_server.c
+++ udp_server.c
@@ -1,56 +1,16 @@
-#include <arpa/inet.h>
-#include <netinet/in.h>
+/* UDP supports unreliable transmission of data, mainly used for Apps */
+/* In UDP, "SOCK_DGRAM" is used*/
+
+/* The following program is a sever side program implemented for UDP */
+
 #include <stdio.h>
-#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
 #include <sys/socket.h>
-#include <unistd.h>
+#include <netinet/in.h>
 
-
-#define BUFLEN 512
-#define NPACK   10
-#define PORT  9930
-
-void diep(char *s)
-{
-    perror(s);
-    exit(1);
-}
-
-int main(void)
-{
-    struct sockaddr_in si_me, si_other;
-    int n;
-    int i;
-    int slen = sizeof(si_other);
-    char buf[BUFLEN];
-
-    if ((n = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)   // "IPPROTO_UDP" the default value is 0    
-        diep ("stocket initialization failed");
-
-    
-    memset ((char*) &si_me, 0, sizeof(si_me));               // initializing a block of memory, value with zero
-
-    si_me.sin_family = AF_INET;                                
-    si_me.sin_port   = htons(PORT);                          // We need famuly, port and address
-    si_me.sin_addr.s_addr = hton1(INADDR_ANY);
-
-    if (bind (n, &si_me, sizeof(si_me)) < 0)
-        diep ("binding error");
-
-    for (i = 0; i < NPACK; i++)
-    {
-        if (recvfrom(n, buf, BUFLEN, 0, &si_other, &slen) < 0)
-            diep("recvfrom()");
-        printf ("Received packet from %s:%d \n Data: %s \n\n",
-            inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port), buf);
-    }
-
-    close(n);
-
-    return 0;
-
-}
-
+#include <netdb.h>
+#include <arpa/inet.h>
 
 
 
