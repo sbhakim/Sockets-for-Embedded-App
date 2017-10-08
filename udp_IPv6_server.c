ed7edb11e5fe056ca8bd6a53ab2809c919c96a0a
sbhakim <sbhakim@kth.se>
2017-10-08 15:28:15 +0300

    udp-implemented

---
+++ udp_IPv6_server.c
@@ -0,0 +1,71 @@
+#include <stdio.h>          /* For fprintf */
+#include <stdlib.h>         
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <string.h>         /* For memcpy */
+#include <netdb.h>
+
+#define PORT 1153
+#define BUFSIZE 2048
+
+
+
+int main (int argc, char **argv)
+{
+    int sock;                           /* This is our socket file descriptor */
+    
+    int  bindVal;
+    struct sockaddr_in6 my_addr;          /* our address */
+    struct sockaddr_in6 rem_addr;         /* remote address */
+
+    socklen_t addrlen = sizeof (rem_addr);
+
+    int recvlen;                        /* #bytes received */
+
+    unsigned char buf[BUFSIZE];         /* receive buffer*/
+
+
+    /* Creating a UDP socket */
+
+    sock = socket (AF_INET6, SOCK_DGRAM, 0);
+
+    if (sock < 0){
+        perror ("Unable to create socket!! \n");
+        return 0;
+    }
+
+    /* Bind the socket to any valid IP address and a specific port */
+
+    memset ((char *) &my_addr, 0, sizeof(my_addr));
+    my_addr.sin6_family = AF_INET6;
+    //    my_addr.sin6_addr = htonl (in6addr_any);
+    my_addr.sin6_addr = in6addr_any;
+    my_addr.sin6_port = htons(PORT);
+
+    bindVal = bind(sock, (struct sockaddr *) &my_addr, sizeof(my_addr));
+
+    if (bindVal < 0)
+    {
+        perror("bind failed");
+        return 0;
+    }
+    
+
+    /* now loop, receiving data and printing what we received */
+
+    for (;;){
+        printf ("Waiting on port %d\n", PORT);
+        recvlen = recvfrom(sock, buf, BUFSIZE, 0, (struct sockaddr *) &rem_addr, &addrlen);
+
+        printf ("\nReceived %d bytes \n", recvlen);
+
+        if (recvlen > 0){
+            buf[recvlen] = 0;
+            printf ("received message: \"%s\" \n", buf);
+        }
+    }
+
+    /* never exits */
+}
+
